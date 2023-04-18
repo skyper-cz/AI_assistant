f194230a5a893b860b8963c7c3eaf5d7b7d3c50f
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2023-04-18 13:44:51 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,20 @@
-import java.time.LocalDateTime;
+//import org.json.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Vítej uživasteli!");
         System.out.println("Dej mi frázi a já ti odpovím!");
         System.out.println("Anglicky PLS");
@@ -14,7 +22,7 @@ public class Main {
         while (true) {
 
             Scanner sc = new Scanner(System.in);
-            String vlozeno = sc.next();
+            String vlozeno = sc.nextLine();
             vlozeno = vlozeno.toLowerCase();
             String[] veta = vlozeno.split(" ");
 
@@ -55,12 +63,34 @@ public class Main {
                             switch (veta[y]) {
                                 case "today":
                                     funguje = true;
+                                    String ipAddress = InetAddress.getLocalHost().getHostAddress(); // Get the IP address of the local machine
+                                    String apiUrl = "https://api.ipgeolocation.io/weather?apiKey=<YOUR_API_KEY>&ip=" + ipAddress; // Replace <YOUR_API_KEY> with your actual API key
+                                    URL url = new URL(apiUrl);
+                                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                                    connection.setRequestMethod("GET");
+                                    connection.setRequestProperty("Content-Type", "application/json");
+
+                                    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                                    String inputLine;
+                                    StringBuffer response = new StringBuffer();
+
+                                    while ((inputLine = in.readLine()) != null) {
+                                        response.append(inputLine);
+                                    }
+                                    in.close();
+
+                                    //JSONObject json = new JSONObject(response.toString());
+                                    //JSONObject current = json.getJSONObject("current");
+
+                                    odpoved = "Today weather is: " + "terrible";
                                     break;
                                 case "tommorow":
                                     funguje = true;
+                                    odpoved = "Tommorow weather is terrible as it is today";
                                     break;
                                 case "weekend":
                                     funguje = true;
+                                    odpoved = "You thing it will change?";
                                     break;
                             }
                         }
@@ -70,12 +100,16 @@ public class Main {
                             switch (veta[y]) {
                                 case "operating":
                                     funguje = true;
+                                    String osName = System.getProperty("os.name").toLowerCase();
+                                    odpoved = "Operating system is: " + osName;
                                     break;
                                 case "goverment":
                                     funguje = true;
+                                    odpoved = "Goverment is defacto totalian as puppet of US";
                                     break;
                                 case "network":
                                     funguje = true;
+                                    odpoved = "Network system is called Internet.";
                                     break;
                             }
                         }
@@ -93,15 +127,15 @@ public class Main {
                                     funguje = true;
                                     break;
                             }
-                            break;
                         }
-                        if (funguje == false) {
-                            System.out.println("error 404 gun not found");
-                        }
-                        else {
-                            System.out.println(odpoved);
-                        }
+                        break;
                 }
+            }
+            if (!funguje) {
+                System.out.println("error 404 gun not found");
+            }
+            else {
+                System.out.println(odpoved);
             }
         }
     }
