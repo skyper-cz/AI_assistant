a1e45e52944ce02c9a8c822cd076970b30f209f2
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2023-04-18 09:15:23 +0200

    i

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,66 @@ public class Main {
         System.out.println("Dej mi frázi a já ti odpovím!");
         System.out.println("Anglicky PLS");
 
-        Scanner sc = new Scanner(System.in);
-        String vlozeno = sc.next();
-        String[] veta = vlozeno.split(" ");
+        while (true) {
 
+            Scanner sc = new Scanner(System.in);
+            String vlozeno = sc.next();
+            vlozeno = vlozeno.toLowerCase();
+            String[] veta = vlozeno.split(" ");
+
+            boolean funguje = false;
+
+            for (int i = 0; i < veta.length; i++) {
+                switch (veta[i]) {
+                    case "time":
+                        for (int y = 0; y < veta.length; y++) {
+                            switch (veta[y]) {
+                                case "now":
+                                    funguje = true;
+                                    break;
+                                case "london":
+                                    funguje = true;
+                                    break;
+                                case "belarus":
+                                    funguje = true;
+                                    break;
+                            }
+                        }
+                        break;
+                    case "weather":
+                        for (int y = 0; y < veta.length; y++) {
+                            switch (veta[y]) {
+                                case "today":
+                                    funguje = true;
+                                    break;
+                                case "tommorow":
+                                    funguje = true;
+                                    break;
+                                case "weekend":
+                                    funguje = true;
+                                    break;
+                            }
+                        }
+                        break;
+                    case "system":for (int y = 0; y < veta.length; y++) {
+                        switch (veta[y]) {
+                            case "operating":
+                                funguje = true;
+                                break;
+                            case "goverment":
+                                funguje = true;
+                                break;
+                            case "network":
+                                funguje = true;
+                                break;
+                        }
+                    }
+                        break;
+                }
+            if (funguje == false){
+                System.out.println("error 404 gun not found");
+            }
+            }
+        }
     }
 }
