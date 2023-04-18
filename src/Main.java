c42f22173e58ad08747e688da90300471539cc7c
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2023-04-18 10:26:29 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
@@ -13,6 +18,8 @@ public class Main {
             vlozeno = vlozeno.toLowerCase();
             String[] veta = vlozeno.split(" ");
 
+            String odpoved = "";
+
             boolean funguje = false;
 
             for (int i = 0; i < veta.length; i++) {
@@ -21,12 +28,23 @@ public class Main {
                         for (int y = 0; y < veta.length; y++) {
                             switch (veta[y]) {
                                 case "now":
+                                    odpoved = "Time now is: " + LocalTime.now();
                                     funguje = true;
                                     break;
                                 case "london":
+                                    ZoneId londonZoneId = ZoneId.of("Europe/London");
+                                    ZonedDateTime londonTime = ZonedDateTime.now(londonZoneId);
+                                    String londonTimeString = londonTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+                                    odpoved = "Current time in London: " + londonTimeString;
                                     funguje = true;
                                     break;
                                 case "belarus":
+                                    ZoneId belarusid = ZoneId.of("Europe/London");
+                                    ZonedDateTime belarustimezone = ZonedDateTime.now(belarusid);
+                                    String belarustime = belarustimezone.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+                                    odpoved = "Current time in Belarus: " + belarustime;
                                     funguje = true;
                                     break;
                             }
@@ -47,24 +65,43 @@ public class Main {
                             }
                         }
                         break;
-                    case "system":for (int y = 0; y < veta.length; y++) {
-                        switch (veta[y]) {
-                            case "operating":
-                                funguje = true;
-                                break;
-                            case "goverment":
-                                funguje = true;
-                                break;
-                            case "network":
-                                funguje = true;
-                                break;
+                    case "system":
+                        for (int y = 0; y < veta.length; y++) {
+                            switch (veta[y]) {
+                                case "operating":
+                                    funguje = true;
+                                    break;
+                                case "goverment":
+                                    funguje = true;
+                                    break;
+                                case "network":
+                                    funguje = true;
+                                    break;
+                            }
                         }
-                    }
                         break;
+                    case "tea":
+                        for (int y = 0; y < veta.length; y++) {
+                            switch (veta[y]) {
+                                case "green":
+                                    funguje = true;
+                                    break;
+                                case "black":
+                                    funguje = true;
+                                    break;
+                                case "fruit":
+                                    funguje = true;
+                                    break;
+                            }
+                            break;
+                        }
+                        if (funguje == false) {
+                            System.out.println("error 404 gun not found");
+                        }
+                        else {
+                            System.out.println(odpoved);
+                        }
                 }
-            if (funguje == false){
-                System.out.println("error 404 gun not found");
-            }
             }
         }
     }
